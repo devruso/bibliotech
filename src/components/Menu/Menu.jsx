c7c878989cd8d93da044bc3c8f5f0f1918bc17cb
ZamboniA <andersonzamboni18@gmail.com>
2023-04-04 14:56:07 -0300

    Foto de perfil na navbar

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,10 +5,11 @@ import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../firebase/auth";
 import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export function Menu() {
   const navigate = useNavigate();
-
+  const user = useContext(AuthContext)
   const resultado = useContext(ThemeContext);
   const temaEscuro = resultado.temaEscuro;
   const alternar = resultado.alternar;
@@ -25,41 +26,44 @@ export function Menu() {
   }
 
   return (
-    <Navbar bg={temaEscuro ? "dark" : "success"}
+    <Navbar   className="navbar"     bg={temaEscuro ? "dark" : "success"}
       variant={temaEscuro ? "dark" : "light"} expand="lg">
       <Container fluid>
         <Navbar.Brand>
-          <Link to="/">
+          <Link  to="/">
             <img src={logoIcon} width="32" alt="Logo" />
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link className="d-flex align-items-center"  as={Link} to="/">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/livros">
+            <Nav.Link className="d-flex align-items-center"  as={Link} to="/livros">
               Livros
             </Nav.Link>
-            <Nav.Link as={Link} to="/emprestimos">
+            <Nav.Link className="d-flex align-items-center"  as={Link} to="/emprestimos">
               Emprestimos
             </Nav.Link>
-            <Nav.Link as={Link}  to="/perfil/usuario">
-              Perfil do usuário
-            </Nav.Link>
 
-            <Nav.Link as={Link} to="/quiz">
+            <Nav.Link  className="d-flex align-items-center"  as={Link} to="/quiz">
               Quiz
             </Nav.Link>
-            
-            <Button variant="outline-light" onClick={alternar}>
+
+            <Button className="d-flex align-items-center" variant="outline-light" onClick={alternar}>
               <img src={iconeBtn} width="16" alt="" />
               Alternar
             </Button>
+            
+            <OverlayTrigger placement="bottom" overlay={<Tooltip id="button-tooltip-2">Perfil de {user.displayName}</Tooltip>}>
+            <Nav.Link className="d-flex align-items-center"  as={Link}  to="/perfil/usuario">
+              <img src={user.photoURL} className="foto-perfil-nav" alt="" />
+            </Nav.Link>
+            </OverlayTrigger>
 
             <OverlayTrigger placement="bottom" overlay={<Tooltip id="button-tooltip-2">Logout</Tooltip>}>
-              <Nav.Link onClick={onLogout}>
+              <Nav.Link className="d-flex align-items-center margim-left"  onClick={onLogout}>
                 <i className="bi bi-box-arrow-right"></i>
               </Nav.Link>
             </OverlayTrigger>
