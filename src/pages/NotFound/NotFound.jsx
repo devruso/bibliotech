7fa2c3805e62d1f2410c4ee0b96cdc38049c3fdf
luiztesta <luiz.eduardo.testa.santos@gmail.com>
2023-04-06 10:30:48 -0300

    fix btn

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -11,48 +11,48 @@ const NotFound = () => {
   const closeModal = () => setShow(false);
   const openModal = () => setShow(true);
 
-       return (
-      <div className="not_found">
+  return (
+    <div className="not_found">
       <h1>404 - Página não encontrada</h1>
       <img src={notFoundImage} alt="Imagem de página não encontrada" />
 
       <div className="button">
 
-      <Button variant="green" href="/" className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Voltar</Button>
-      <Button variant="green" href="/login" className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Login</Button>
-      <Button variant="dark" onClick={openModal} className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Reportar </Button>
+        <Button variant="green" href="/" className="btn-notFound btn-primary btn-lg active" role="button" aria-pressed="true">Voltar</Button>
+        <Button variant="green" href="/login" className="btn-notFound btn-primary btn-lg active" role="button" aria-pressed="true">Login</Button>
+        <Button variant="dark" onClick={openModal} className="btn-notFound btn-primary btn-lg active" role="button" aria-pressed="true">Reportar </Button>
       </div>
 
       <Modal show={show} onHide={closeModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>Reportar Erro</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
+        <Modal.Header closeButton>
+          <Modal.Title>Reportar Erro</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
 
           <Form>
-          <Form.Group
-                className="mb-3"
-                controlId="textarea"
-              >
-                <Form.Label>Descreva o erro:</Form.Label>
-                <Form.Control as="textarea" rows={3} />
-              </Form.Group>
-            </Form>
+            <Form.Group
+              className="mb-3"
+              controlId="textarea"
+            >
+              <Form.Label>Descreva o erro:</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
 
-            </Modal.Body>
-          <Modal.Footer>
-            <Button  variant="secondary" onClick={closeModal}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={closeModal}>
-              Save
-            </Button>
-          </Modal.Footer>
-        </Modal>
-    
-      </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={closeModal}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-    );
+    </div>
+
+  );
 }
 
 export default NotFound;
