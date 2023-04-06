106b13b61656e54f7bd7a4a02c2185805c0e60eb
luiztesta <luiz.eduardo.testa.santos@gmail.com>
2023-04-06 09:21:01 -0300

    fix rotas

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import { PoliticaPrivacidade } from "./pages/PoliticaPrivacidade/PoliticaPrivaci
 import { Ajuda } from "./pages/Ajuda/Ajuda";
 import { Chat } from "./pages/Chat/Chat";
 import { RecuperacaoSenha } from "./pages/RecuperacaoSenha/RecuperacaoSenha";
-import {EmailVerification} from "./pages/EmailVerification/EmailVerification"
+import { EmailVerification } from "./pages/EmailVerification/EmailVerification"
 import { Loader } from "./components/Loader/Loader";
 import { BlogLista } from "./pages/BlogLista/BlogLista";
 
@@ -48,7 +48,7 @@ export function App() {
     });
   }, []);
 
-    if (loading) {
+  if (loading) {
     return <Loader />;
   }
 
@@ -72,17 +72,18 @@ export function App() {
                 <Route path="/emprestimos/editar/:id" element={<EditarEmprestimo />} />
                 <Route path="/quiz" element={<BemVindo></BemVindo>}>  </Route>
                 <Route path="/quiz/perguntas" element={<Quiz></Quiz>}></Route>
-                <Route path="/perfil/usuario" element={<PerfilUsuario/>} />
-                <Route path="/footer" element={<Footer/>} />
-                <Route path="/chat" element={<Chat/>}/>
-                <Route path="/blog" element={<BlogLista></BlogLista>}></Route>
-                </Route>
+                <Route path="/perfil/usuario" element={<PerfilUsuario />} />
+                <Route path="/footer" element={<Footer />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/blog" element={<BlogLista />} />
+                <Route path="/ajuda" element={<Ajuda />} />
+              </Route>
               <Route path="/login" element={<Login />} />
-              <Route path="/recuperacao-senha" element={<RecuperacaoSenha />}/>
+              <Route path="/recuperacao-senha" element={<RecuperacaoSenha />} />
               <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/confirmaremail" element={<EmailVerification/>} />
-              <Route path="*" element={<NotFound/>} />
-              
+              <Route path="/confirmaremail" element={<EmailVerification />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="/privacidade" element={<PoliticaPrivacidade />} />
             </Routes>
           </BrowserRouter>
         </AuthContext.Provider>
